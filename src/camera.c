89557a5bd277a8dcc6aa6721f6fe48a398bbdc9c
Idan Shoham <idan@hitachi-id.com>
2021-11-26 19:33:48 -0700

    Fixed a subtle memory leak.

--- src/camera.c
+++ src/camera.c
@@ -702,6 +702,7 @@ void* ProcessNewImageInThread( void* params )
 #endif
 
   _IMAGE* prevImage = NULL;
+  int allocatedPrevImage = 0;
   if( NOTEMPTY( prevFile )
       && cam->recentImage!=NULL
       && NOTEMPTY( cam->recentImage->fileName )
@@ -719,6 +720,7 @@ void* ProcessNewImageInThread( void* params )
     Notice( "Parsing JPEG for previous image from %s - %s / %s", cam->nickName, cam->folderPath, prevFile );
 #endif
     prevImage = ImageFromJPEGFile2( cam->nickName, cam->folderPath, prevFile );
+    allocatedPrevImage = 1;
     }
 
   if( prevImage==NULL || prevImage->data==NULL )
