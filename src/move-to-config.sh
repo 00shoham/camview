1514596e34e6737661d2d2b3b942394267eaa9ee
Idan Shoham <00shoham@gmail.com>
2022-10-08 09:26:47 -0700

    script to copy binaries to config folder

---
+++ src/move-to-config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BINS=\
+  brightenimage\
+  cam-archive\
+  cam-config\
+  cam-monitor\
+  cam-status
+  cam-view\
+  download-recordings\
+  imagediff\
+  jpegtest\
+  single-image\
+  testimagelist\
+
+cp $(BINS) ../../v/src
